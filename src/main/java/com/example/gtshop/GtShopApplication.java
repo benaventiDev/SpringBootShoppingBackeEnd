87635e1725463e9543dcef04b81cfd35cdd8e7fc
benaventiLinux <201021212@ingenieria.usac.edu.gt>
2024-03-23 20:49:18 -0600

    Init Commit

---
+++ src/main/java/com/example/gtshop/GtShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.gtshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtshop/GtShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
